2aeb87653cc859445f21496142f11b6229902c69
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-03 17:26:27 -0700

    navbar customized commit

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <nav>
-            <div className='link-container'>
+            <div className='link-container font-bold'>
                 <NavLink className={({ isActive }) => isActive ? "active-link" : "link"} to='./'>Home</NavLink>
                 <NavLink className={({ isActive }) => isActive ? "active-link" : "link"} to='./review'>Review Page</NavLink>
                 <NavLink className={({ isActive }) => isActive ? "active-link" : "link"} to='./dashboard'>Dashboard</NavLink>
