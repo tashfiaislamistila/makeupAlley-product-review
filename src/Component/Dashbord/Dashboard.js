f76b241fc7b08d215e76f7b5a1b60ddc8239d7e3
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-04 12:20:45 -0700

    alignment fixing in dashbord commit

--- src/Component/Dashbord/Dashboard.js
+++ src/Component/Dashbord/Dashboard.js
@@ -13,66 +13,72 @@ const Dashboard = () => {
             <div>
                 <h1 className='text-6xl text-neutral-800 mt-6'>Dashboard</h1>
             </div>
-            <div>
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="month" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="investment" stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
-                </LineChart>
-                <AreaChart
-                    width={500}
-                    height={400}
-                    data={data}
-                    margin={{
-                        top: 10,
-                        right: 30,
-                        left: 0,
-                        bottom: 0,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="month" />
-                    <YAxis />
-                    <Tooltip />
-                    <Area type="monotone" dataKey="sell" stackId="1" stroke="#8884d8" fill="#8884d8" />
-                    <Area type="monotone" dataKey="revenue" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-                    <Area type="monotone" dataKey="investment" stackId="1" stroke="#ffc658" fill="#ffc658" />
-                </AreaChart>
-                <BarChart
-                    width={500}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 20,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="month" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="sell" stackId="a" fill="#8884d8" />
-                    <Bar dataKey="investment" stackId="a" fill="#82ca9d" />
-                    <Bar dataKey="revenue" fill="#ffc658" />
-                </BarChart>
+            <div className='grid grid-cols-3 mt-8'>
+                <div>
+                    <LineChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="investment" stroke="#8884d8" activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+                    </LineChart>
 
+                </div>
+                <div>
+                    <AreaChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 0,
+                            bottom: 0,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Area type="monotone" dataKey="sell" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                        <Area type="monotone" dataKey="revenue" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                        <Area type="monotone" dataKey="investment" stackId="1" stroke="#ffc658" fill="#ffc658" />
+                    </AreaChart>
+                </div>
+                <div>
+                    <BarChart
+                        width={500}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="sell" stackId="a" fill="#8884d8" />
+                        <Bar dataKey="investment" stackId="a" fill="#82ca9d" />
+                        <Bar dataKey="revenue" fill="#ffc658" />
+                    </BarChart>
+                </div>
             </div>
         </div>
 
