185d67c31309ab17d2f475b65b7f03f053ec56ce
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-04 00:13:40 -0700

    rating show with font awesome commit

--- src/Component/Review/Review.js
+++ src/Component/Review/Review.js
@@ -5,14 +5,17 @@ import ReviewComponent from '../ReviewComponent/ReviewComponent';
 const Review = () => {
     const [reviews] = useReviews();
     return (
-        <div className='grid grid-cols-3 gap-4'  >
-            {
-                reviews.map(review => <ReviewComponent
-                    key={review.id}
-                    review={review}
-                ></ReviewComponent>)
-            }
+        <div>
+            <h1>This is Our review page</h1>
+            <div className='grid grid-cols-3 justify-items-center gap-4 pt-10 '  >
+                {
+                    reviews.map(review => <ReviewComponent
+                        key={review.id}
+                        review={review}
+                    ></ReviewComponent>)
+                }
 
+            </div>
         </div>
     );
 };
--- src/Component/ReviewComponent/ReviewComponent.js
+++ src/Component/ReviewComponent/ReviewComponent.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import Rating from 'react-rating';
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const ReviewComponent = (props) => {
     console.log(props.review)
     const { image, name, rating, review } = props.review;
     return (
-        <div className='p-10 '>
-            <div className='max-w-sm rounded overflow-hidden shadow-lg '>
-                <img className='object-scale-down h-48 w-96 bg-cover bg-center ' src={image} alt="" />
-                <h2 className='font-bold text-xl mb-2 p-5'>Name: {name}</h2>
-                <h2 className='font-bold text-xl mb-2'>Ratings: {rating}</h2>
-                <h2 className='text-gray-700 text-base font-bold p-6  '>Review: {review.length < 200 ? review : review.slice(0, 200) + "...See More"}</h2>
-
-
-            </div>
-
-
+        <div className='max-w-sm rounded overflow-hidden shadow-lg p-10 '>
+            <img className='object-scale-down h-48 w-96 bg-cover bg-center ' src={image} alt="" />
+            <h2 className='font-bold text-xl mb-2 p-5'>Name: {name}</h2>
+            <Rating
+                initialRating={rating}
+                emptySymbol={<FontAwesomeIcon icon={faStar} />}
+                fullSymbol={<FontAwesomeIcon style={{ color: 'goldenrod' }} icon={faStar} />}
+                readonly
+            ></Rating>
+            <h2 className='text-gray-700 text-base font-bold p-6'><h2>Review:</h2> {review.length < 200 ? review : review.slice(0, 200) + "...See More"}</h2>
 
 
         </div>
+
+
     );
 };
 
